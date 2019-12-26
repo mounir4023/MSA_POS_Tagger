dbacc0fa139f8703c8b565aa63a7fe1c33739ecb
devnir <dev.mounir@protonmail.com>
2019-12-26 22:16:27 +0100

    CODE CLEANING

--- corpus.py
+++ corpus.py
@@ -19,37 +19,3 @@ for s in sents:
         if t == '' or t == ' ':
             s["tokens"].remove(t)
 
-"""
-for s in sents[10:15]:
-    print(s["tokens"])
-    print("start: ",s["tokens"][0]," end: ",s["tokens"][-1])
-    pass
-print(" ")
-
-for sent in sents[10:15]:
-    #if sent["tokens"][-1] == ['؟']:
-        #sent["tokens"] = reversed(sent["tokens"]) 
-        #pass
-    #else:
-    #elif sent["tokens"][-1] == ['.']:
-        sent["tokens"][:-1] = reversed(sent["tokens"][:-1]) 
-
-for s in sents[10:15]:
-    print(s["tokens"])
-    print("start: ",s["tokens"][0]," end: ",s["tokens"][-1])
-    pass
-"""
-
-for s in sents:
-    if s["tokens"][-1] == '؟':
-        print(s["tokens"])
-        print("start: ",s["tokens"][0]," end: ",s["tokens"][-1])
-
-
-
-"""
-for s in sents:
-    for t in s["tokens"]:
-        if t == '' or t == ' ':
-            print("\nempty char at: ",s["num"])
-"""
