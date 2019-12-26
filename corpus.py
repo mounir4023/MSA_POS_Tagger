50b00e981ca967b5b41204b4ab80f2cd728bb179
devnir <dev.mounir@protonmail.com>
2019-12-26 16:51:13 +0100

    catching multipe sentences not ending with .

--- corpus.py
+++ corpus.py
@@ -6,13 +6,14 @@ from lxml import etree
 
 root = etree.parse("corpus.xml")
 
-for e in root.xpath("/CORPUS/Phrase")[:100]:
+for e in root.xpath("/CORPUS/Phrase"):
 
-    if e[2].text.split(" ")[-1] != ".\n":
+    if not (e[2].text.split(" ")[-1] == ".\n" and e[2].text.split(" ")[-1] == "\n" ):
         print("num phrase: ",e[0].text)
-        print("text: ",e[1].text)
+        #print("text: ",e[1].text)
         print("tokens: ",e[2].text)
-        print("tags: ",e[3].text)
+        print("\nsplit: ", e[2].text.split(" "))
+        #print("tags: ",e[3].text)
         #print(e[2].text.split(" "))
 
 
