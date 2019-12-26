4952ad25fcc5592c6b362bf1f874f5cf46279875
devnir <dev.mounir@protonmail.com>
2019-12-26 16:04:01 +0100

    phrase 41 fixed

---
+++ trash/phrase 41 not ending with dot_newline/fixed/corpus.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as et
+import xml.dom.minidom as md
+
+from lxml import etree
+
+
+root = etree.parse("corpus.xml")
+
+for e in root.xpath("/CORPUS/Phrase")[:100]:
+
+    if e[2].text.split(" ")[-1] != ".\n":
+        print("num phrase: ",e[0].text)
+        print("text: ",e[1].text)
+        print("tokens: ",e[2].text)
+        print("tags: ",e[3].text)
+        #print(e[2].text.split(" "))
+
+
+
