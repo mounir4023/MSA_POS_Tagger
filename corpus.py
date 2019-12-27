856285fa9bafddcde438a1acb6be8b0c3ee70a6c
devnir <dev.mounir@protonmail.com>
2019-12-27 16:54:19 +0100

    added <start> token, code cleaned

--- corpus.py
+++ corpus.py
@@ -2,19 +2,23 @@ from lxml import etree
 import re
 
 
+# reading corpus from xml
 root = etree.parse("corpus.xml")
 sents = [ ]
 
+# xml to dict for each sentence 
 for s in root.xpath("/CORPUS/Phrase"):
     tokens = re.sub(r'\s+',' ',s[2].text)
     tags = re.sub(r'\s+',' ',s[3].text)
     sents.append({
             'num':s[0].text,
+            'len':s[5].text,
             'raw':s[1].text,
             'tokens':tokens.split(" "),
             'tags':tags.split(" "),
         })
 
+# cleaning empty tokens and tags
 for s in sents:
     for t in s["tokens"]:
         if t == '' or t == ' ':
@@ -23,21 +27,13 @@ for s in sents:
         if t == '' or t == ' ':
             s["tags"].remove(t)
 
+# adding the START state
 for s in sents:
-    if len(s["tokens"]) != len(s["tags"]) -1 :
-        print(s["num"])
+    s["tokens"][0:0] = ['<START>']
 
-"""
-s = sents[20]
-print(s["tags"])
 
-for t in s["tokens"]:
-    print(t)
-print("\n",s["tokens"],"\n")
-
-s["tokens"][0:0] = ['<START>']
-
-for t in s["tokens"]:
-    print(t)
-print("\n",s["tokens"],"\n")
-"""
+# random test
+import random
+s = random.choice(sents)
+for i in range(0,int(s["len"])+1):
+    print(s["tokens"][i],"\t",s["tags"][i])
