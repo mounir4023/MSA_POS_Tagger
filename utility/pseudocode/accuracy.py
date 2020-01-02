7913eea5022304a82ea3767c33b25d50e46e1763
mounir4023 <dev.mounir@protonmail.com>
2020-01-02 23:37:50 +0100

    FINAL EVALUATION TEST DONE

--- utility/pseudocode/accuracy.py
+++ utility/pseudocode/accuracy.py
@@ -3,43 +3,44 @@ from hmm import get_HMM
 from viterbi import decode
 from evaluation import eval_model
 import random
+import csv
 import pandas as pd
 
-# setting up data
 data = random.shuffle(get_data("corpus.xml"))
-train_set = get_data("corpus.xml")[:3300]
-test_set = get_data("corpus.xml")[3300:]
+train_set = get_data("corpus.xml")[:3000]
+test_set = get_data("corpus.xml")[3000:]
 lexicon = get_lexicon("lexicon.txt")
-
-# training the model
 model = get_HMM(train_set, lexicon)
 
-# evaluation on train data
-results = eval_model(train_set, model)
+results = eval_model(train_set[:500], model)
 
+"""
 df = pd.DataFrame(results["confusion"]).fillna(0)
-df.to_csv('train_confusion.csv')
+df.to_csv('confusion.csv')
 c = [ item[0] for item in results["worst_tags"][:10] ]
 s = [ item[0] for item in results["worst_codes"][:10] ]
-print("################# TRAININ EVALUATION ################")
 print("\n")
 print("sent acc: ",results["sent_accuracy"])
 print("     acc: ",results["accuracy"])
-print("\nCommon confusions:\n")
+print("\nCommon confusions:")
 print(results["confusion"].tabulate( conditions = c, samples = s ) )
+"""
 
+"""
+print("train set: 3000")
+print("sent acc: ",results["sent_accuracy"])
+print("     acc: ",results["accuracy"])
 
-# evaluation on test data
 results = eval_model(test_set, model)
 
-df = pd.DataFrame(results["confusion"]).fillna(0)
-df.to_csv('test_confusion.csv')
-c = [ item[0] for item in results["worst_tags"][:10] ]
-s = [ item[0] for item in results["worst_codes"][:10] ]
-print("\n\n\n################# TEST EVALUATION ################")
-print("\n")
+print("test set: 1000")
 print("sent acc: ",results["sent_accuracy"])
 print("     acc: ",results["accuracy"])
-print("\nCommon confusions:\n")
-print(results["confusion"].tabulate( conditions = c, samples = s ) )
+"""
 
+"""
+#print("\nworst_tags\n",results["worst_tags"])
+#print("\nworst_words:\n",results["worst_words"])
+#print("confusion matrix:")
+#print(results["confusion"].tabulate())
+"""
